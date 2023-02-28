cf61e9e4d11057dcba21d85ecd5ce396be5991ea
carlin dev <116854351+carlin68401@users.noreply.github.com>
2023-02-28 08:58:31 -0300

    Add files via upload

---
+++ api/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getStudents")]
+        public async Task<ActionResult<Student>> GetStudents()
+        {
+            var students = new List<Student>()
+            {
+                new Student()
+                {
+                    id= 1,
+                    name= "carlin dev",
+                   age=30,
+                   city= "nova odessa"
+                },
+                {
+                new Student()
+                {
+                    id= 2,
+                    name= "jao dev",
+                   age=40,
+                   city= "nova odessa"
+                }
+
+        }
+        };
+            return Ok(students);
+    }
+    }
+}
---
+++ api/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace api
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+
+        public int age { get; set; }
+
+        public string city { get; set; } = string.Empty;
+
+    }
+}
